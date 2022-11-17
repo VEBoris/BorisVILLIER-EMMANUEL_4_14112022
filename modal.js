d6dee68071a6b21b6126c9f4ee2bdf7e39fb3a56
VEBoris <Visuya@outlook.fr>
2022-11-18 00:05:03 +0100

    add close button

--- modal.js
+++ modal.js
@@ -12,6 +12,16 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Form Input Elements
+const firstName = document.getElementById("#first");
+const lastName = document.getElementById("#last");
+const email = document.getElementById("#email");
+const birth = document.getElementById("#birthdate");
+
+// Form elements
+const close = document.querySelector(".close");
+const btnSubmit = document.getElementsByClassName(".btn-submit");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +30,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close button
+close.addEventListener("click", function (event) {
+  modalbg.style.display = "none";
+});
