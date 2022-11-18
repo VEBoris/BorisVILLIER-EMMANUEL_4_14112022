51dbf652537fc253cc689ec52e1eb21c2ce08e91
VEBoris <Visuya@outlook.fr>
2022-11-18 01:02:28 +0100

    Add message error champs vide & message perso

--- modal.js
+++ modal.js
@@ -1,11 +1,5 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// Navbar
+const menu = document.querySelector(".main-navbar .icon");
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -13,29 +7,29 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // Form Elements
-const firstName = document.getElementById("#first");
-const lastName = document.getElementById("#last");
-const email = document.getElementById("#email");
-const birth = document.getElementById("#birthdate");
-const participation = document.getElementById("#quantity");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birth = document.getElementById("birthdate");
+const participation = document.getElementById("quantity");
 const locations = document.getElementsByName("location");
-const checkbox1 = document.getElementsByClassName(".checkbox-icon");
-const checkbox2 = document.getElementById("#checkbox2");
+const checkbox = document.getElementsByClassName(".checkbox-icon");
 const close = document.querySelector(".close");
-const btnSubmit = document.getElementsByClassName(".btn-submit");
+const btnSubmit = document.getElementsByClassName("btn-submit");
+const form = document.querySelector("form");
 
 // Empty Elements Forms
-const emptyFirstName = document.getElementById("#firstname-empty");
-const emptyLastName = document.getElementById("#lastname-empty");
-const emptyEmail = document.getElementById("#mail-empty");
-const emptyBirth = document.getElementById("#birth-empty");
-const emptyPartipation = document.getElementById("#participation-empty");
-const emptyLocations = document.getElementsById("location-empty");
+const emptyFirstName = document.getElementById("firstname-empty");
+const emptyLastName = document.getElementById("lastname-empty");
+const emptyEmail = document.getElementById("mail-empty");
+const emptyBirth = document.getElementById("birth-empty");
+const emptyPartipation = document.getElementById("participation-empty");
+const emptyLocations = document.getElementById("location-empty");
+const emptyCheckbox = document.getElementById("checkbox-empty");
 
 // Regex
-const regexName = /^[a-zA-Z\-àâçéèêëîïôûùüÿñæœ']{2,}$/;
-const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-const regexParticipation = /^(0?[0-9]|[0-9][0-9])$/;
+const regexName = /^[a-zA-Z\-àâçéèêëîïôûùüÿñæœ']{1}$/;
+const regexEmail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -46,68 +40,85 @@ function launchModal() {
 }
 
 // Close button
-close.addEventListener("click", function (event) {
+close.addEventListener("click", function () {
   modalbg.style.display = "none";
 });
 
-// Error message
-function errorMessage(value) {
-  value.style.color = "red";
-  value.style.fontSize = "0.7em";
-  value.style.border = "2px solid red";
+// Menu navbar
+menu.addEventListener("click", editNav);
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
-// Verfication champs de saisie - Prénom
+// Style (css) error message
+function errorMessage(value) {
+  value.style.color = "red";
+  value.style.fontSize = "0.6em";
+  value.style.margin = "0.5em 0 0.8em 0.5em";
+}
+
+// Vérification saisie Prénom
 function checkFirstName() {
-  if (firstName == "") {
-    errorMessage(emptyFirstName).innerHTML = "Veuillez saisir votre Prénom";
-    return false;
-  } else if (regexName == false) {
-    errorMessage(emptyFirstName).innerHTML =
-      "Veuillez saisir un minimum de 2 caractère";
+  if (!firstName.value) {
+    emptyFirstName.innerHTML = "Veuillez saisir votre Prénom";
     emptyFirstName.style.display = "block";
+    errorMessage(emptyFirstName);
+    return false;
+  } else if (firstName.value.search(regexName) == false) {
+    emptyFirstName.innerHTML = "Veuillez saisir un minimum de 2 caractère";
+    emptyFirstName.style.display = "block";
+    errorMessage(emptyFirstName);
   } else {
     emptyFirstName.style.display = "none";
     return true;
   }
 }
 
-// Verfication champs de saisie - Nom
+// Vérification saisie Nom
 function checkLastName() {
-  if (lastName == "") {
-    errorMessage(emptyLastName).innerHTML = "Veuillez saisir votre Nom";
+  if (!lastName.value) {
+    emptyLastName.innerHTML = "Veuillez saisir votre Nom";
     emptyLastName.style.display = "block";
     return false;
-  } else if (regexName == false) {
-    errorMessage(emptyLastName).innerHTML =
-      "Veuillez saisir un minimum de 2 caractère";
+  } else if (lastName.value.search(regexName) == false) {
+    emptyLastName.innerHTML = "Veuillez saisir un minimum de 2 caractère";
     emptyLastName.style.display = "block";
+    errorMessage(emptyLastName);
   } else {
     emptyLastName.style.display = "none";
     return true;
   }
 }
 
+// Vérification saisie Email
 function checkEmail() {
-  if (email == "") {
-    errorMessage(emptyEmail).innerHTML = "Veuillez saisir votre Email";
+  if (!email.value) {
+    emptyEmail.innerHTML = "Veuillez saisir votre Email";
     emptyEmail.style.display = "block";
+    errorMessage(emptyEmail);
     return false;
-  } else if (regexEmail == false) {
-    errorMessage(emptyEmail).innerHTML =
-      "Veuillez saisir une adresse email valide";
+  } else if (regexEmail.test(email.value) == null) {
+    emptyEmail.innerHTML = "Veuillez saisir une adresse email valide";
     emptyEmail.style.display = "block";
+    errorMessage(emptyEmail);
+    return false;
   } else {
     emptyEmail.style.display = "none";
     return true;
   }
 }
 
+// Vérification saisie Date de Naissance
 function checkBirth() {
-  if (birth == "") {
-    errorMessage(emptyBirth).innerHTML =
-      "Veuillez saisir votre Date de Naissance";
+  if (!birth.value) {
+    emptyBirth.innerHTML = "Veuillez saisir votre Date de Naissance";
     emptyBirth.style.display = "block";
+    errorMessage(emptyBirth);
     return false;
   } else {
     emptyBirth.style.display = "none";
@@ -115,16 +126,18 @@ function checkBirth() {
   }
 }
 
+// Vérification saisie Participation
 function checkParticipation() {
-  if (partipation == "") {
-    errorMessage(emptyPartipation).innerHTML =
-      "Veuillez saisir votre participation ";
+  if (!participation.value) {
+    emptyPartipation.innerHTML =
+      "Veuillez renseigner le nombre de participation ";
     emptyPartipation.style.display = "block";
+    errorMessage(emptyPartipation);
     return false;
-  } else if (regexParticipation == false) {
-    errorMessage(emptyPartipation).innerHTML =
-      "Veuillez saisir un nombre valide";
+  } else if (participation.value > 99) {
+    emptyPartipation.innerHTML = "Veuillez saisir un nombre valide";
     emptyPartipation.style.display = "block";
+    errorMessage(emptyPartipation);
   } else {
     emptyPartipation.style.display = "none";
     return true;
@@ -132,23 +145,40 @@ function checkParticipation() {
 }
 
 function checkLocations() {
-  if (locations == "") {
-    errorMessage(emptyLocations).innerHTML = "Veuillez choisir une ville";
-    emptyLocations.style.display = "block";
-    return false;
-  } else {
-    emptyLocations.style.display = "none";
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
+      emptyLocations.innerHTML = "Veuillez choisir une ville";
+      emptyLocations.style.display = "block";
+      errorMessage(emptyLocations);
+      return false;
+    } else {
+      emptyLocations.style.display = "none";
+      return true;
+    }
   }
 }
 
-//  Verfication sur le Form
-function validate() {
+function checkCondition() {
+  if (!checkbox.checked) {
+    emptyCheckbox.innerHTML = "Veuillez accepter les conditions d'utilisations";
+    emptyCheckbox.style.display = "block";
+    errorMessage(emptyCheckbox);
+    return false;
+  } else {
+    emptyCheckbox.style.display = "none";
+    return true;
+  }
+}
+
+//  Verfication des saisies sur le Form
+function validationForm() {
   let resultFirstName = checkFirstName();
   let resultLastName = checkLastName();
   let resultEmail = checkEmail();
   let resulBirth = checkBirth();
   let resultParticipation = checkParticipation();
   let resultLocations = checkLocations();
+  let resultCondition = checkCondition();
 
   if (
     resultFirstName &&
@@ -156,9 +186,26 @@ function validate() {
     resultEmail &&
     resulBirth &&
     resultParticipation &&
-    resultLocations
+    resultLocations &&
+    resultCondition
   ) {
     modalbg.style.display = "none";
     return true;
   }
 }
+
+
+// Submit si les champs sont remplis sinon verif Form
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (form.checkValidity === true) {
+    setTimeout(validationForm, 1000);
+    modalbg.style.display = "none";
+    return true;
+  } else {
+    validationForm();
+  }
+
+  console.log("submit");
+});
